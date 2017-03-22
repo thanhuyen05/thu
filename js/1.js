bf69fdbbe6af711545e9eb5a59f15745d0159cde
thanhuyen05 <thanhuyenletran@gmail.com>
2017-03-22 14:02:43 +0700

    update tintuc

--- js/1.js
+++ js/1.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
+	//js for web ca nhan
+
 	new WOW().init();
 	$(".clickme").click(function(){
 		$("header nav div.navbar-toi").toggleClass("hien");
 	});
 	$(window).scroll(function(){
 		var t = $(window).scrollTop();
-		if(t>66)
+		if(t>64)
 		{
 			$("header nav.navbar.navbar-light.bg-faded").addClass("bura");
 		}
@@ -13,12 +15,30 @@ $(document).ready(function() {
 			$("header nav.navbar.navbar-light.bg-faded").removeClass("bura");
 		}
 	});
-	$("header nav.navbar.navbar-light.bg-faded ul.nav.navbar-nav li.nav-item a").click(function(){
-		var r = $(this).attr("href");
-		var t=$(r).offset().top;
-		t=t+500;
-		$(window).animate({top:t},function(){
-
-		});
+	$("header nav.navbar.navbar-light.bg-faded ul.nav.navbar-nav li.nav-item a.nav-link").click(function(){
+		var target = $( $(this).attr('href') );
+        $('.body').animate({
+            scrollTop:target
+        },2000);
 	});
+	//js cho web tin tuc
+	//scroll chuot
+
+			// for trangnhat2
+		$('#scroll-trangnhat2').enscroll({
+		    horizontalScrolling: true,
+		    verticalTrackClass: 'vertical-track1',
+		    verticalHandleClass: 'vertical-handle1',
+    	});
+
+    	//for khuenmai
+    	$('#scroll-khuyenmai').enscroll({
+		    horizontalScrolling: true,
+		    verticalTrackClass: 'vertical-track2',
+		    verticalHandleClass: 'vertical-handle2',
+    	});
+    //menu reponsive
+    $("header button.navbar-toggler-right.left").click(function(){
+    	$("#my-menu").toggleClass("hienra");
+    });
 });
